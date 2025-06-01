035de018c3599abbde86adaf53f490b7e5e8ccf2
Hisanori Ito <86972518+hisanori-ito@users.noreply.github.com>
2025-06-02 01:08:54 +0900

    feat: 🎸 add signup environment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
+const PORT = 3000;
+const config = require('./config');
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to express!');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   // console.log('Start server port:3000');
+});
+
+// sign up
+app.post('/signup', (req, res) => {
+  const payload = {
+    firstname: req.body.firstname,
+    familyname: req.body.familyname,
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  const token = jwt.sign(payload, config.jwt.secret, config.jwt.options);
+
+  const body = {
+    firstname: req.body.firstname,
+    familyname: req.body.familyname,
+    email: req.body.email,
+    password: req.body.password,
+    token: token,
+  };
+
+  res.status(200).json(body);
 });
