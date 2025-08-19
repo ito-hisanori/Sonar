e6ebe75003f9d5e68cef8254bb2f2f06faf5a4d6
Hisanori Ito <86972518+hisanori-ito@users.noreply.github.com>
2025-08-19 15:01:21 +0900

    feat: 🎸 add directory

---
+++ frontend/sonar/src/app/record/page.jsx
@@ -0,0 +1,8 @@
+export default function Record() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium">Record</h1>
+      <h1>Diving Record</h1>
+    </div>
+  );
+}
